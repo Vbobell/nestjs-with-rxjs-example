1b40d5c02001b4f41ea2d0fa2c46d9d14367cb99
Vbobell <vinicius.leite@gupy.com.br>
2022-07-07 14:23:26 -0300

    fix(user-task): add unit test to repository

--- src/user-task/infra/repository/memory/user-task.repository.spec.ts
+++ src/user-task/infra/repository/memory/user-task.repository.spec.ts
@@ -3,17 +3,50 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserTaskRepositoryMemory } from '@app/user-task/infra/repository/memory/user-task.repository';
 
 describe('UserTaskRepositoryMemory', () => {
-  let service: UserTaskRepositoryMemory;
+  let repository: UserTaskRepositoryMemory;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserTaskRepositoryMemory],
     }).compile();
 
-    service = module.get<UserTaskRepositoryMemory>(UserTaskRepositoryMemory);
+    repository = module.get<UserTaskRepositoryMemory>(UserTaskRepositoryMemory);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('When get user task list', () => {
+    test('Then get list with success', (done) => {
+      repository.getUserTasks(1).subscribe((result) => {
+        expect(result).toEqual({
+          tasks: [
+            {
+              title: 'Task 3',
+              description: 'Description task 3',
+            },
+          ],
+          user: {
+            id: 1,
+          },
+        });
+
+        done();
+      });
+    });
+
+    test('Then get empty list', (done) => {
+      repository.getUserTasks(3).subscribe((result) => {
+        expect(result).toEqual({
+          tasks: [],
+          user: {
+            id: 3,
+          },
+        });
+
+        done();
+      });
+    });
   });
 });
